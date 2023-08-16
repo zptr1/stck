3ebbf8905ab3e0ff79df6c7786dab15c495c34b6
zptr1 <instance004@gmail.com>
2023-08-16 17:16:26 +0300

    handle argv

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import { printProgramAst } from "./src/util/prettyprint";
 import { File } from "./src/shared/location";
 import { Parser } from "./src/parser";
 import { Lexer } from "./src/lexer";
+import { existsSync } from "fs";
 import chalk from "chalk";
 import plib from "path";
 
 async function run(file: File) {
-  console.log("[INFO] Running", chalk.gray(plib.resolve(file.path)));
+  console.log("[INFO] Running", chalk.gray(file.path));
   console.log("[INFO] Tokenizing");
 
   const lexer = new Lexer(file);
@@ -21,4 +22,11 @@ async function run(file: File) {
   printProgramAst(ast);
 }
 
-run(File.read("test.stck"));
+if (process.argv.length < 3) {
+  console.log("stck v0.0.1");
+  console.error(`Usage: ... <file>`);
+} else if (!existsSync(process.argv[2])) {
+  console.error(`No such file or directory: ${process.argv[2]}`);
+} else {
+  run(File.read(plib.resolve(process.argv[2])));
+}
