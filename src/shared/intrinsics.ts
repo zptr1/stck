61c393deb09f382fcbe225420458445aa6cdf6d3
zptr1 <instance004@gmail.com>
2023-08-16 20:39:19 +0300

    Rename symbol intrinsics to words for consistency

--- src/shared/intrinsics.ts
+++ src/shared/intrinsics.ts
@@ -14,13 +14,13 @@ function addIntrinsic(name: string, ins: DataType[], outs: DataType[]) {
   });
 }
 
-addIntrinsic("+",     [DataType.Int, DataType.Int],  [DataType.Int]);
-addIntrinsic("-",     [DataType.Int, DataType.Int],  [DataType.Int]);
-addIntrinsic("*",     [DataType.Int, DataType.Int],  [DataType.Int]);
-addIntrinsic("/%",    [DataType.Int, DataType.Int],  [DataType.Int]);
-addIntrinsic("<",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
-addIntrinsic("=",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
-addIntrinsic(">",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
-addIntrinsic("dup",   [DataType.Any],                [DataType.Any, DataType.Any]);
-addIntrinsic("swap",  [DataType.Any, DataType.Any],  [DataType.Any, DataType.Any]);
-addIntrinsic("print", [DataType.Int],                []);
+addIntrinsic("add",    [DataType.Int, DataType.Int],  [DataType.Int]);
+addIntrinsic("sub",    [DataType.Int, DataType.Int],  [DataType.Int]);
+addIntrinsic("mul",    [DataType.Int, DataType.Int],  [DataType.Int]);
+addIntrinsic("divmod", [DataType.Int, DataType.Int],  [DataType.Int]);
+addIntrinsic("lt",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
+addIntrinsic("eq",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
+addIntrinsic("gt",     [DataType.Int, DataType.Int],  [DataType.Boolean]);
+addIntrinsic("dup",    [DataType.Any],                [DataType.Any, DataType.Any]);
+addIntrinsic("swap",   [DataType.Any, DataType.Any],  [DataType.Any, DataType.Any]);
+addIntrinsic("print",  [DataType.Int],                []);
